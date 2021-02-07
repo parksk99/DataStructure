2d63fbd190465b24042da0fae151cc76d29ba0c8
parksk99 <qkrtjdrb99@naver.com>
2021-02-07 19:55:37 +0900

    have to study

---
+++ test.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+class A{
+	private:
+		int a;
+		friend std::ostream& operator<<(std::ostream&, A&);
+	public:
+		A(int tmp = 0) : a(tmp){};
+		A(A& tmp){
+			a = tmp.a;
+		}
+		A f(){
+			A tmp(100);
+			return tmp;
+		}
+};
+
+std::ostream& operator<<(std::ostream& os, A& ref){
+	os<<ref.a;
+	return os;
+}
+int main(){
+	A a(10);
+	A &b(a);
+//	std::cout<<b;
+	std::cout<<b.f();
+	
+	return 0;
+}
